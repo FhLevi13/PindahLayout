7083eab0edd4b9239a00d76381dbf99d5c21d606
FhLevi13 <alifvahlephi46@gmail.com>
2022-10-03 10:30:55 +0700

    03/10/2022

--- app/src/main/java/com/lepii/pindahlayout/MainActivity.java
+++ app/src/main/java/com/lepii/pindahlayout/MainActivity.java
@@ -2,13 +2,26 @@ package com.lepii.pindahlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnPindah;
+    TextView tvSelamatDatang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvSelamatDatang = findViewById(R.id.tv_selamat_datang);
+        btnPindah = findViewById(R.id.btn_pindah);
+
+        btnPindah.setOnClickListener(view -> {
+            Intent pindah = new Intent(MainActivity.this, Second.class);
+            startActivity(pindah);
+        });
     }
 }
